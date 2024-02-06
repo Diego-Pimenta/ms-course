358b4f8c39b35ab5fa8d658659437290bef78585
Diego Pimenta <diegopimenta888@gmail.com>
2024-02-06 11:50:40 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-api-gatewat-zuul/src/main/java/com/diego/hrapigatewayzuul/HrApiGatewatZuulApplication.java
@@ -0,0 +1,13 @@
+package com.diego.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatewatZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewatZuulApplication.class, args);
+	}
+
+}
